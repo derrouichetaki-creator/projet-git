30b021c2092e632d8a640cb31df89b49bed0cd01
taki <derrouichetaki@gmail.com>
2026-05-26 20:26:46 +0000

    Fix script logs

--- scripts/analyse_logs.sh
+++ scripts/analyse_logs.sh
@@ -4,7 +4,7 @@ DATE=$(date)
 echo "Rapport de sécurité - $DATE" > reports/report.txt
 echo "" >> reports/report.txt
 echo "Tentatives SSH échouées :" >> reports/report.txt
-grep "Failed password" /var/log/auth.log >> reports/report.txt
+grep "Failed password" /var/log/auth.log >> reports/report.txt || echo "Aucune tentative" >> reports/report.txt
 echo "" >> reports/report.txt
 echo "Erreurs HTTP 404 :" >> reports/report.txt
-grep "404" /var/log/nginx/access.log >> reports/report.txt
+grep "404" /var/log/nginx/access.log >> reports/report.txt || echo "Aucune erreur 404" >> reports/report.txt
